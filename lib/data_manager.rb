f6abf6811836be027af82760365e460155acf736
rafahg <r.hgar78@gmail.com>
2020-10-28 12:03:33 +0000

    #single_webs method and @webs variable added, tested in featured and unit test, test are passing

--- lib/data_manager.rb
+++ lib/data_manager.rb
@@ -1,11 +1,12 @@
 require './lib/file_manager.rb'
 
 class DataManager
-  attr_reader :data, :data_pairs
+  attr_reader :data, :data_pairs, :webs
 
   def initialize(log)
     @data = FileManager.new(log)
     @data_pairs = []
+    @webs = []
   end
 
   def organize_data_in_pairs
@@ -13,5 +14,14 @@ class DataManager
     data_pairs.map! { |pair| pair[0].split(" ") }
   end
   
-
+  def single_webs
+    data_pairs.each do |web|
+      webs.push(web[0]).uniq!    
+    end
+  end
+  
+  a = DataManager.new('webserver.log')
+  a.organize_data_in_pairs
+  a.single_webs
+  p a.webs
 end
--- spec/data_manager_spec.rb
+++ spec/data_manager_spec.rb
@@ -12,5 +12,25 @@ describe DataManager do
       a = DataManager.new('webserver.log')
       expect(a.organize_data_in_pairs.length).to eq 500
     end
+    it '#organize_data_in_pairs returns @data_pairs with 2 elements in it.' do
+      a = DataManager.new('webserver.log')
+      expect(a.organize_data_in_pairs[0].length).to eq 2
+    end
+    it '#data_pairs first element is as expected' do 
+      a = DataManager.new('webserver.log')
+      a.organize_data_in_pairs
+      expect(a.data_pairs[0][0]).to eq '/help_page/1'
+    end
+    it '#data_pairs second element is as expected' do
+      a = DataManager.new('webserver.log')
+      a.organize_data_in_pairs
+      expect(a.data_pairs[0][1]).to eq '126.318.035.038'
+    end
+    it '#single_webs return the list of uniq webs in @webs' do
+      a = DataManager.new('webserver.log')
+      a.organize_data_in_pairs
+      a.single_webs
+      expect(a.webs.length).to eq 6
+    end
   end
 end
